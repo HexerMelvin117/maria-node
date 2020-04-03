2b760630e1ed87149a1ee759e552cebee9a70b40
Melvin Rosales <39774579+HexerMelvin117@users.noreply.github.com>
2020-04-03 16:13:15 -0600

    added create, delete and read functions

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,48 @@
 const app = module.exports = require('express')()
 
+const queries = require('../queries/index')
+
 app.get('/', (req, res) => {
-    res.send('You found me')
+    res.json({ message: "You found me!" })
 })
+
+// Get all tests from Database
+app.get('/test', async (req, res) => {
+    let myResult = await queries.queryMytable()
+    res.json(myResult)
+})
+
+// Add test to database
+app.post('/addtest', async (req, res) => {
+    let test = req.body
+    console.log(test)
+
+    try {
+        let postTest = await queries.addToTable(test.id, test.desc);
+        res.json({ message: "Record added succesfully", id: test.id, desc: test.desc})
+    } catch (err) {
+        res.json({errorMessage: err})
+    }
+})
+
+// Get one test
+app.get('/tests/:id', async (req, res) => {
+    try {
+        let getOneTest = await queries.getOneTest(req.params.id)
+        res.json(getOneTest)
+    } catch (err) {
+        res.json({errorMessage: err})
+    }
+})
+
+// Delete one test/record
+app.delete('/tests/:id', async (req, res) => {
+    try {
+        let deleteTest = await queries.deleteTest(req.params.id)
+        res.send(deleteTest)
+    } catch (err) {
+        res.json({errorMessage: err})
+    }
+})
+
+// Update a test
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const port = 8080
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const mariaRoutes = require('./routes/index')
 
 app.use(cors())
+app.use(bodyParser.json())
 app.use(mariaRoutes)
 
 app.listen(port, () => {
